07e62f6c6e9a1b1071560e9e0fc18c05e0fff242
Vladimir Sabev <vdsabev@gmail.com>
2017-11-02 15:20:41 +0200

    Recommit

--- src/config.js
+++ src/config.js
@@ -1,26 +1,23 @@
-module.exports = ({ production }) => {
+module.exports = (options) => {
   const path = require('path');
 
   const cmless = require(path.join(process.cwd(), 'package.json')).cmless;
   const cmlessExtends = require(path.join(process.cwd(), cmless.extends));
-  const webpackConfig = cmlessExtends({ production });
-
-  const config = require('./config');
-  const cmlessConfig = config(webpackConfig, { production, ...cmless });
+  const webpackConfig = cmlessExtends(options);
 
   return {
-    devtool: production ? false : 'inline-source-map',
+    devtool: options.production ? false : 'inline-source-map',
     context: process.cwd(),
     // entry: {
     //   index: `./${APP_DIR}/index.js`,
     //   vendor
     // },
-    // output: {
-    //   publicPath: '/',
-    //   path: path.resolve(BUILD_DIR),
-    //   filename: '[name].[chunkhash].js',
-    //   sourceMapFilename: '[name].js.map'
-    // },
+    output: {
+      publicPath: '/',
+      path: path.join(process.cwd(), cmless.build),
+      filename: '[name].[chunkhash].js',
+      sourceMapFilename: '[name].js.map'
+    },
     resolve: {
       extensions: ['.js', '.jsx', '.css']
     },
