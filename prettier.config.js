5cb8d9c1d62e014173e135b3b7f3e22a4caf1051
Vlad Sabev <vdsabev@gmail.com>
2018-06-09 04:27:01 +0300

    Revert "Switched to no semicolons by default"
    This reverts commit 6b2cbd319ad7b02ce66b1b4822f10f82e77bc970.

--- prettier.config.js
+++ prettier.config.js
@@ -2,10 +2,10 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  semi: false,
+  semi: true,
   singleQuote: true,
   trailingComma: 'es5',
   bracketSpacing: true,
   jsxBracketSameLine: false,
   arrowParens: 'always',
-}
+};
--- src/rules/script.js
+++ src/rules/script.js
@@ -2,13 +2,13 @@ const getScriptRule = (options) => ({
   test: /\.jsx?$/,
   loader: 'babel-loader',
   exclude: [/node_modules/],
-})
+});
 
 const getTypeScriptRule = (options) => ({
   test: /\.tsx?$/,
   loader: 'ts-loader',
   exclude: [/node_modules/],
-})
+});
 
-exports.getScriptRule = getScriptRule
-exports.getTypeScriptRule = getTypeScriptRule
+exports.getScriptRule = getScriptRule;
+exports.getTypeScriptRule = getTypeScriptRule;
