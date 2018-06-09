fa1f76b7c705537195dcf799398a127374b00ec6
Vladimir Sabev <vdsabev@gmail.com>
2018-06-09 03:56:38 +0300

    Switched to no semicolons by default

--- prettier.config.js
+++ prettier.config.js
@@ -2,10 +2,10 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  semi: true,
+  semi: false,
   singleQuote: true,
   trailingComma: 'es5',
   bracketSpacing: true,
   jsxBracketSameLine: false,
   arrowParens: 'always',
-};
+}
